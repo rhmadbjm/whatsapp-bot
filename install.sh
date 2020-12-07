5c52a9d16062e1e42bf615334e79a1aff784c9e2
rhmadbjm <75595860+rhmadbjm@users.noreply.github.com>
2020-12-07 14:09:54 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+apt update
+apt upgrade
+apt install
+pkg update && pkg upgrade
+pkg install wget -y
+pkg install ffmpeg -y
+pkg install nodejs -y
+pkg install wget
+pkg install ffmpeg
+pkg install nodejs
+npm i -g cwebp
+npm i -g ytdl 
+npm i
+npm i got
+node index.js
